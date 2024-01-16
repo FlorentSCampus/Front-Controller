579c21848be7413208036e29588b12bb8e22d968
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-01-16 11:14:08 +0100

    create index page

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<?php
+$metaTitle = 'fornt_controller';
+?>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $metaTitle ?></title>
+</head>
+
+<body>
+    <?php require_once 'src/header.php' ?>
+    <?php require_once 'src/content.php' ?>
+    <?php require_once 'src/footer.php' ?>
+
+</body>
