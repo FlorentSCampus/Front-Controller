f5cc95c34439358eea252b12d69b04e3c2abcd46
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-01-17 16:09:56 +0100

    add link to style.css

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ $filter = filter_input(INPUT_GET, "page", FILTER_SANITIZE_ENCODED);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $metaTitle ?></title>
 
+    <link rel="stylesheet" href="css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 
