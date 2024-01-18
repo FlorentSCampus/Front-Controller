98ead7ea1deb9be2a109e6212a3daad5df8c314d
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-01-18 14:25:32 +0100

    add array of error message

---
+++ src/error.php
@@ -0,0 +1,11 @@
+<?php
+$errors = array(
+    "404" => require_once "404.php",
+    "gender" => "Veuillez sélectionner un entrée dans la liste ci-dessous",
+    "firstName" => "Veuillez renseigner votre prénom",
+    "lastName" => "Veuillez renseigner votre nom",
+    "email" => "Veuillez renseigner votre email",
+    "radio" => "Veuillez sélectionner une option",
+    "message" => "Veuillez renseigner votre message",
+    "messageLetters" => "Votre message doit contenir au moins 5 lettres"
+);
