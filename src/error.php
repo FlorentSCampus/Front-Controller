c39c90e445784cccdfd846aa4882dd3e8d9d4c39
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-01-19 09:40:07 +0100

    change double quote to simple quote

--- src/error.php
+++ src/error.php
@@ -1,10 +1,10 @@
 <?php
 $errors = array(
-    "gender" => "Veuillez sélectionner un entrée dans la liste ci-dessous",
-    "firstName" => "Veuillez renseigner votre prénom",
-    "lastName" => "Veuillez renseigner votre nom",
-    "email" => "Veuillez renseigner votre email",
-    "radio" => "Veuillez sélectionner une option",
-    "message" => "Veuillez renseigner votre message",
-    "messageLetters" => "Votre message doit contenir au moins 5 lettres"
+    'gender' => 'Veuillez sélectionner un entrée dans la liste ci-dessous',
+    'firstName' => 'Veuillez renseigner votre prénom',
+    'lastName' => 'Veuillez renseigner votre nom',
+    'email' => 'Veuillez renseigner votre email',
+    'radio' => 'Veuillez sélectionner une option',
+    'message' => 'Veuillez renseigner votre message',
+    'messageLetters' => 'Votre message doit contenir au moins 5 lettres'
 );
--- src/processing.php
+++ src/processing.php
@@ -1,22 +1,22 @@
 <?php
-$date = date("__Y_m_d__H_i_s");
-$filename = "contact";
-$filename = $filename . $date . ".txt";
+$date = date('__Y_m_d__H_i_s');
+$filename = 'contact';
+$filename = $filename . $date . '.txt';
 
 $validers = array(
-    "email" => FILTER_VALIDATE_EMAIL
+    'email' => FILTER_VALIDATE_EMAIL
 );
 
 $isValid = filter_input_array(INPUT_POST, $validers);
 
 if ($isValid) {
     $cleaners = array(
-        "gender" => FILTER_DEFAULT,
-        "firstName" => FILTER_SANITIZE_SPECIAL_CHARS,
-        "lastName" => FILTER_SANITIZE_SPECIAL_CHARS,
-        "email" => FILTER_SANITIZE_EMAIL,
-        "radio" => FILTER_DEFAULT,
-        "message" => FILTER_SANITIZE_SPECIAL_CHARS
+        'gender' => FILTER_DEFAULT,
+        'firstName' => FILTER_SANITIZE_SPECIAL_CHARS,
+        'lastName' => FILTER_SANITIZE_SPECIAL_CHARS,
+        'email' => FILTER_SANITIZE_EMAIL,
+        'radio' => FILTER_DEFAULT,
+        'message' => FILTER_SANITIZE_SPECIAL_CHARS
     );
 }
 
@@ -27,7 +27,7 @@ $isEmpty = isEmpty($result, $length);
 
 if (!$isEmpty) {
     $data = print_r($result, true);
-    file_put_contents("./data/" . $filename, $data);
+    file_put_contents('./data/' . $filename, $data);
 }
 
 function isEmpty($array, $length)
